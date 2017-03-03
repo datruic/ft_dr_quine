c2dcaa4ef84025ff31bb3fcefc763fd8d7cf07ce
Parth POKAR <ppokar@e2r9p6.42.fr>
2017-03-03 18:16:25 +0100

    #define S needs to be updated rest seems fine

--- ex02/Sully.c
+++ ex02/Sully.c
@@ -1,12 +1,15 @@
 int i = 5;
 #include <stdio.h>
 #include <stdlib.h>
-#define S "int i = %d;%c#include <stdio.h>%c#include <stdlib.h>%c#define S %c%s%c%cint main()%c{%c%cchar cmd[992];char path[992];FILE *fp;%c%cif (i < 0) exit(0);sprintf(path, %cSully_%cd.c%c, i);fp = fopen(path, %cw%c);%c%cfprintf(fp,S,i-1,10,10,10,34,S,34,10,10,/*nl and tap for cmd*/10,9,10,9,34,37,34,34,34,10,9,10,9,34,37,37,37,34,/*nl and tap for return*/10,9,10);%c%cfclose(fp);sprintf(cmd, %cclang -o Sully_%cd %cs; ./Sully_%cd%c,i,path,i);system(cmd);%c%creturn 0;%c}"
+#define S "int i = %d;%c#include <stdio.h>%c#include <stdlib.h>%c#define S %c%s%c%cint main()%c{%c%cchar cmd[992];char path[992];FILE *fp;%c%cif (i < 0) exit(0);sprintf(path, %cSully_%cd.c%c, i);fp = fopen(path, %cw%c);%c%cfprintf(fp,S,i-1,10,10,10,34,S,34,10,10,/*nl and tap for cmd*/10,9,10,9,34,37,34,34,34,10,9,10,9,34,37,37,37,34,/*nl and tap for return*/10,9,10);%c%cfclose(fp);//sprintf(cmd, %cclang -o Sully_%cd %cs; ./Sully_%cd%c,i,path,i);system(cmd);%c%creturn 0;%c}"
 int main()
 {
-	char cmd[992];char path[992];FILE *fp;
-	if (i < 0) exit(0);sprintf(path, "Sully_%d.c", i);fp = fopen(path, "w");
+	char cmd[992];char path[992];char cmd2[992];FILE *fp;
+	if (i < 0) exit(0);sprintf(path, "Sully_%d.c", i);sprintf(cmd2,"./Sully_%d",i);fp = fopen(path, "w");
 	fprintf(fp,S,i-1,10,10,10,34,S,34,10,10,/*nl and tap for cmd*/10,9,10,9,34,37,34,34,34,10,9,10,9,34,37,37,37,34,/*nl and tap for return*/10,9,10);
-	fclose(fp);sprintf(cmd, "clang -o Sully_%d %s; ./Sully_%d",i,path,i);system(cmd);
+	//fclose(fp);sprintf(cmd, "clang -o Sully_%d %s; ./Sully_%d",i,path,i);system(cmd);
+	fclose(fp);sprintf(cmd, "clang -o Sully_%d %s",i,path);system(cmd);
+	fp = fopen(path, "r+");fseek(fp,8,SEEK_SET);fprintf(fp,"%d;\n",i);fclose(fp);
+	//system(cmd2);//next file print needs to be updated needs to be changed.
 	return 0;
 }
